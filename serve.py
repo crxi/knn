ce48c4bd421ef5b6e2c8aa15677d2a3598807203
me <me@home>
2024-02-03 15:20:03 +0800

    updated server.py

--- serve.py
+++ serve.py
@@ -1,5 +1,13 @@
+import argparse
 import uvicorn
 import streamsync.serve
+
+parser = argparse.ArgumentParser(description='serve.py')
+
+parser.add_argument('--loglevel', type=str, default='info',
+                    help='Log level for the server script')
+
+args = parser.parse_args()
 
 app_path = "." # . for current working directory
 mode = "run" # run or edit
@@ -8,6 +16,6 @@ asgi_app = streamsync.serve.get_asgi_app(app_path, mode)
 
 uvicorn.run(asgi_app,
     host="0.0.0.0",
-    port=3005,
-    log_level="info",
+    port=3333,
+    log_level=args.loglevel,
     ws_max_size=streamsync.serve.MAX_WEBSOCKET_MESSAGE_SIZE)
